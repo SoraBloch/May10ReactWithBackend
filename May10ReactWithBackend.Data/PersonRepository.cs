1820d1f92c4cc04e1f2d673bc8fb150b7fc3509c
Sora Bloch <sorahindybloch@gmail.com>
2023-05-16 00:41:25 -0400

    changed code

--- May10ReactWithBackend.Data/PersonRepository.cs
+++ May10ReactWithBackend.Data/PersonRepository.cs
@@ -34,7 +34,7 @@ namespace May10ReactWithBackend.Data
         public void Delete(int id)
         {
             using var context = new PeopleDbContext(_connectionString);
-            context.Database.ExecuteSqlInterpolated($"DELETE FROM People WHERE Id = ${id}");
+            context.Database.ExecuteSqlInterpolated($"DELETE FROM People WHERE Id = {id}");
         }
         public void DeleteAll(List<int> ids)
         {
